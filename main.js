e506bba002eec6f3bf4e596daca8cf2a2b663a06
rokmc1256 <skh77017@naver.com>
2023-03-05 00:09:51 +0900

    first commit

--- main.js
+++ main.js
@@ -7,19 +7,29 @@ const rightArrow = document.querySelector('.fa-chevron-right');
 const images = document.querySelector('.images');
 
 
-
+leftArrow.style.opacity = 0;
 leftArrow.addEventListener('click', () => {
     if(indexNum > 0) {
+        rightArrow.style.opacity = 1;
         positionValue += IMAGE_WIDTH;
         images.style.transform = `translateX(${positionValue}px)`;
         indexNum--;
+    }
+    
+    if(indexNum === 0) {
+        leftArrow.style.opacity = 0;
     }
 })
 
 rightArrow.addEventListener('click', () => {
     if(indexNum < 2) {
+        leftArrow.style.opacity = 1;
         positionValue -= IMAGE_WIDTH;
         images.style.transform = `translateX(${positionValue}px)`;
         indexNum++;
+    }
+    
+    if(indexNum === 2) {
+        rightArrow.style.opacity = 0;
     }
 })
