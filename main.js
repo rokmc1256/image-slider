6d9b6ee6cc3185dcdcd2220d891f93c0a056abe9
rokmc1256 <skh77017@naver.com>
2023-03-06 22:50:43 +0900

    소스코드 수정

--- main.js
+++ main.js
@@ -5,14 +5,21 @@ const IMAGE_WIDTH = 640;
 const leftArrow = document.querySelector('.fa-chevron-left');
 const rightArrow = document.querySelector('.fa-chevron-right');
 const images = document.querySelector('.images');
+const icon1 = document.querySelector('.icon1');
+const icon2 = document.querySelector('.icon2');
+const icon3 = document.querySelector('.icon3');
+const icon = document.querySelectorAll('.icon');
 
 
 leftArrow.style.opacity = 0;
+icon1.style.backgroundColor = '#2b2d42';
 leftArrow.addEventListener('click', () => {
     if(indexNum > 0) {
         rightArrow.style.opacity = 1;
         positionValue += IMAGE_WIDTH;
         images.style.transform = `translateX(${positionValue}px)`;
+        icon[indexNum - 1].style.backgroundColor = '#2b2d42';
+        icon[indexNum].style.backgroundColor = '#e5e5e5';
         indexNum--;
     }
     
@@ -26,6 +33,8 @@ rightArrow.addEventListener('click', () => {
         leftArrow.style.opacity = 1;
         positionValue -= IMAGE_WIDTH;
         images.style.transform = `translateX(${positionValue}px)`;
+        icon[indexNum + 1].style.backgroundColor = '#2b2d42';
+        icon[indexNum].style.backgroundColor = '#e5e5e5';
         indexNum++;
     }
     
